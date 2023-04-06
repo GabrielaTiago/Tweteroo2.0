d28e44b220ba71061506fad6cff359228bf85e0e
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-04-05 22:47:02 -0300

    :sparkles: feat: Creates the tweet repository to get the tweets by the username

--- src/main/java/com/tweteroo/api/repositories/TweetsRepository.java
+++ src/main/java/com/tweteroo/api/repositories/TweetsRepository.java
@@ -1,9 +1,12 @@
 package com.tweteroo.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tweteroo.api.models.Tweet;
 
 public interface TweetsRepository extends JpaRepository<Tweet, Long> {
     
+    List<Tweet> findByUsername(String username);
 }

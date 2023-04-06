476ff08a2b1860c3441b5221ab57a4117302d31c
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-04-05 22:19:21 -0300

    :sparkles: feat: Creates the tweet controller to get the tweets with pagination

--- src/main/java/com/tweteroo/api/controllers/TweetsController.java
+++ src/main/java/com/tweteroo/api/controllers/TweetsController.java
@@ -4,13 +4,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dtos.TweetDTO;
+import com.tweteroo.api.models.Tweet;
 import com.tweteroo.api.services.TweetsService;
 
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -27,4 +34,9 @@ public class TweetsController {
         return ResponseEntity.status(HttpStatus.CREATED).body("OK");
     }
 
+    @GetMapping
+    public List<Tweet> getTweets(
+            @PageableDefault(page = 0, size = 5, sort = "id", direction =  Sort.Direction.DESC) Pageable page) {
+        return service.getTweets(page);
+    }
 }
